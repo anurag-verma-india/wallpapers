94fde254c43feccec31f54aaa1583511d6ed21c2
Anurag Verma <anuragsoniemail@gmail.com>
2023-11-30 19:10:05 +0530

    2023 11 30

--- spotlight-dl.bat
+++ spotlight-dl.bat
@@ -1,3 +1,3 @@
-xcopy /s C:\Users\kk\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\kk\Documents\GitHub\wallpapers\windows-spotlight
-cd C:\Users\kk\Documents\GitHub\wallpapers\windows-spotlight
+xcopy /s "C:\Users\Anurag Verma\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "C:\Users\Anurag Verma\Documents\GitHub\wallpapers\windows-spotlight"
+cd "C:\Users\Anurag Verma\Documents\GitHub\wallpapers\windows-spotlight"
 ren * *.jpg
